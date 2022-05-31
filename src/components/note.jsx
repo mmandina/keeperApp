4f9708e8c8c1d11b223d8a6993cab811ba901010
Max <maxwell.mandina@gmail.com>
2022-05-31 21:17:41 +1100

    note update

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Note() {
-  return<div className='note'><h1>title</h1><p>contents</p></div>
+  return<div className='note'><h1>Note Title</h1><p>Note Contents</p></div>
 }
