f0d6c2a4ab5c132decf79cb86c805e25b06d7aee
Max <maxwell.mandina@gmail.com>
2022-06-01 19:15:24 +1100

    used MAP to iterate through notes object to generate new notes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import Footer from './footer'
 import Note from './note'
 import Heading from './header'
+import { notes } from '../notes'
+
+function createNote(noteItem) {
+  return <Note
+    title={noteItem.title}
+    content= {noteItem.content}
+  />
+}
+
 export default function app() {
   return <div><Heading />
-  <Note />
+  {notes.map(createNote)}
   <Footer /></div>
 
  }
